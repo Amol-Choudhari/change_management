1cb2d85310ce55a55386a045597bf045b33605fe
Akash Pralhad Thakre <akashthakreofficial@gmail.com>
2022-06-29 19:21:31 +0530

    Commercial Changes

--- src/Model/Table/MCommodityCategoryTable.php
+++ src/Model/Table/MCommodityCategoryTable.php
@@ -6,9 +6,15 @@ use App\Model\Model;
 use Cake\ORM\TableRegistry;
 
 class MCommodityCategoryTable extends Table{
-	
+
 	var $name = "MCommodityCategory";
 	var $useTable = "MCommodityCategory";
+
+
+		public function	getCategoryName($id){
+			return $detail = $this->find('all')->select(['category_name'])->where(['category_code IS' => $id])->first();
+		}
+
 }
 
 ?>
--- src/Model/Table/MCommodityTable.php
+++ src/Model/Table/MCommodityTable.php
@@ -3,11 +3,16 @@ namespace app\Model\Table;
 	use Cake\ORM\Table;
 	use App\Model\Model;
 	use Cake\ORM\TableRegistry;
-	
+
 class MCommodityTable extends Table{
-	
+
 	var $name = "MCommodity";
 	var $useTable = "m_commodity";
+
+	public function	getCommodityName($id){
+		return $this->find('all')->select(['commodity_name'])->where(['commodity_code IS' => $id])->first();
+	}
+
 }
 
 ?>
